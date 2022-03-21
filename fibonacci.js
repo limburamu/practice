8f7a859925ea8bdc76b99b3f63dd61ac1b60ce3f
limburamu <limburamu23@gmail.com>
2022-03-21 07:46:46 +0545

    bug fixed

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,12 @@
 fibo = 0;
 f1 = 0
 f2 = 1;
+console.log(f1);
+console.log(f2);
 for(i = 0; i < 10; i ++)
 {
-    console.log(fibo);
     fibo = f1 + f2;
     f1 = f2;
     f2 = fibo;
+    console.log(fibo);
 }
--- prime.js
+++ prime.js
@@ -1,16 +1,18 @@
-let n = 7;
-flag = 1;
+let n = 9;
+let flag = 1;
 for(i = 2; i < n; i++)
 {
     if(n % i == 0)
     {
+        console.log(i);
         flag = 0;
+        break;
     }
 }
 
-if(flag = 1)
+if(flag == 1)
 {
-    console.log(n + 'is a prime number');
+    console.log(n + ' is a prime number');
 }else{
-    console.log(n + ' is composit prime number');
+    console.log(n + ' is composite number');
 }
